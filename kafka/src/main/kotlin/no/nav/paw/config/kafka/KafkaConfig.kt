87eab3335871f5c7a5590a0ee1f2fcb36e0723c8
Nils Martin Sande <nils.martin.sande@nav.no>
2024-02-21 14:10:55 +0100

    La til støtte for vanlige consumers uten kafka-streams

--- kafka/src/main/kotlin/no/nav/paw/config/kafka/KafkaConfig.kt
+++ kafka/src/main/kotlin/no/nav/paw/config/kafka/KafkaConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.paw.config.kafka
 
 const val KAFKA_CONFIG_WITH_SCHEME_REG = "kafka_configuration_schemareg.toml"
+const val KAFKA_STREAMS_CONFIG_WITH_SCHEME_REG = "kafka_streams_configuration_schemareg.toml"
 const val KAFKA_CONFIG = "kafka_configuration.toml"
 
 data class KafkaConfig(
